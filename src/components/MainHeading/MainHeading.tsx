89a4666ad5d15647133ac7a0dde600ac37acbf05
Vlad Dobrij <vlad.dobrij123@gmail.com>
2024-05-06 23:20:44 +0300

    header animation fix

--- src/components/MainHeading/MainHeading.tsx
+++ src/components/MainHeading/MainHeading.tsx
@@ -10,14 +10,18 @@ const MainHeading = () => {
                 if (entry.isIntersecting) {
                     let divs = Array.from(entry.target.children) as [HTMLElement]
                     divs.forEach((div, id) => {
-                        setTimeout(() => {
+                        const overlay = div.lastElementChild
+                        if (overlay) {
+                            div.style.animationDelay = 300 * (id + 1)+1500 + "ms"
                             div.classList.add(css.inViewportHeadingAnimation);
-                        }, 300 * id + 1)
+                            overlay.style.animationDelay = `${300 * (id + 1)+1000}ms, ${300 * (id + 1) + 2000}ms`
+                            overlay.classList.add(css.temp);
+                        }
                     })
                 }
             });
         };
-        const Obs = new IntersectionObserver(inViewport,{
+        const Obs = new IntersectionObserver(inViewport, {
             threshold: .5,
         });
         document.querySelectorAll('[data-heading]').forEach(el => {
@@ -33,19 +37,26 @@ const MainHeading = () => {
         >
             <div className={`meSection`}
                  style={{marginTop: '15vw'}}
-                 data-title={"Vlad Dobrinov,s"}
+                // data-title={"Vlad Dobrinov,"}
             >
                 Vlad Dobrinov,
+                <div className={'overlay'}/>
             </div>
+
             <div className={`meSection`}
-                 data-title={"highly driven"}
+                // data-title={"highly driven"}
+
             >
                 highly driven
+                <div className={'overlay'}/>
+
             </div>
             <div className={`meSection`}
-                 data-title={"Front Dev"}
+                // data-title={"Front Dev"}
             >
                 Front Dev
+                <div className={'overlay'}/>
+
             </div>
         </section>
     );
